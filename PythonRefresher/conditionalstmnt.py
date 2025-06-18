8057ac5eb11d24b040c98d5a39ee98a18c927a4a
Timothy Wamalwa <timwamalwa@gmail.com>
2025-06-18 17:11:13 +0300

    added condition statement

---
+++ PythonRefresher/conditionalstmnt.py
@@ -0,0 +1,10 @@
+# Conditional statements in python are used to execute code based on certain conditions.
+
+age = 20
+if age < 18:
+    print("You are a minor.")
+elif age < 65:
+    print("You are an adult.")
+else:
+    print("You are a senior citizen.")
+
